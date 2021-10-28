0d96428878f9360be0004e600bd2d535f7203a18
maximilien vitiello <maxiquick@outlook.fr>
2021-10-28 16:32:08 +0200

    VER 0.6.9

--- Assets/_GAME/WriteBlueScreen.cs
+++ Assets/_GAME/WriteBlueScreen.cs
@@ -12,6 +12,7 @@ public class WriteBlueScreen : MonoBehaviour
     private string textFull = "A problem has been detected and windows has been shut down to prevent damage to your computer.\n\nUNMOUNTABLE_BOOT_VOLUME\n\nIf this is the first time you've seen this error screen, \nrestart your computer. If this screen appear again, follow\nthese steps:\n\nCheck to make sure any new hardware or software is properly installed.\nIf this is a new installation, ask your hardware or software manufacturer\nfor any windows updates you might need.\n\nIf problems continue, disable or remove any newly installed hardware\n or software. Disable BIOS memory options such as caching or shadowing.\nIf you need to use Safe Mode to remove or disable components, restart\nyour computer, press F8 to select Advanced Startup Options, and then\nSelect Safe Mode.\n\n Technical Information:\n\n*** STOP: 0x000000ED (0x80F128D0, 0xc000009c, 0x00000000)";
     private string currentText = "";
     private bool isAnswer = false;
+    private bool isLevelScene = false;
 
     [SerializeField, Tooltip("time to write one letter")]
     private float letterTimeToWrite = 0.05f;
@@ -31,7 +32,12 @@ public class WriteBlueScreen : MonoBehaviour
             if(writeTime >= letterTimeToWrite)
             {
                 currentText += textFull[i];
-                questionUI.TextZone.text = currentText;
+
+                if(isLevelScene)
+                    questionUI.TextZone.text = currentText;
+                else
+                    text.text = currentText;
+                
 
                 writeTime = 0;
                 i++;
@@ -52,6 +58,7 @@ public class WriteBlueScreen : MonoBehaviour
 
     public void SetText(QuestionAsset questionAsset)
     {
+        isLevelScene = true;
         questionUI.TextZone.transform.localPosition = new Vector3(3.909f, -212.809f, 0);
         currentText = "";
         this.questionAsset = questionAsset;
